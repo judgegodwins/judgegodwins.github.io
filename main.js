57d69e1829f3de7bbe7c9b8be8e62f0bfe8b2bd8
Judge Godwins <judgegodwins@gmail.com>
2020-05-03 20:51:32 +0100

    fetch-test

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 var button = document.querySelector('.custom-button');
 var boxFromDown = document.querySelector('.box-down');
+var submit = document.querySelector('.submit');
+var email = document.querySelector('#email'),
+    name = document.querySelector('#name'),
+    message = document.querySelector('#msg')
 
 
 button.addEventListener('click', function(e) {
     boxFromDown.classList.toggle('up');
     this.classList.toggle('opened')
+})
+
+submit.addEventListener('click', function(e) {
+    e.preventDefault();
+    fetch('https://judgeportfolio.herokuapp.com/new_message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email.value,
+            name: name.value,
+            message: message.value
+        })
+    })
 })
