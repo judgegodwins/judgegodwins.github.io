d52e83a8e32e1b9d2e357ee1eb66185401d96226
Judge Godwins <judgegodwins@gmail.com>
2020-05-04 07:38:04 +0100

    add name attr for msg form

--- main.js
+++ main.js
@@ -2,28 +2,27 @@ var button = document.querySelector('.custom-button');
 var boxFromDown = document.querySelector('.box-down');
 var submit = document.querySelector('.submit');
 var email = document.querySelector('#email'),
-    name = document.querySelector('#name'),
-    message = document.querySelector('#msg')
-
+    message = document.querySelector('#msg');
+var name = document.getElementById('name')
 
 button.addEventListener('click', function(e) {
     boxFromDown.classList.toggle('up');
     this.classList.toggle('opened')
 })
 
-submit.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log(email.value, ' ', name.value, ' ', message.value)
-    fetch('https://judgeportfolio.herokuapp.com/new_message', {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email.value,
-            name: name.value,
-            message: message.value
-        })
-    });
-})
+// submit.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log(email.value, ' ', name.value, ' ', message.value)
+//     fetch('https://judgeportfolio.herokuapp.com/new_message', {
+//         method: 'POST',
+//         mode: 'no-cors',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             email: email.value,
+//             name: document.getElementById('name').value,
+//             message: message.value
+//         })
+//     });
+// })
