c33b6248ecd9357bb726c64c5aa272543aaa91e4
Judge Godwins <judgegodwins@gmail.com>
2020-05-23 21:29:46 +0100

    test

--- main.js
+++ main.js
@@ -42,7 +42,8 @@ submit.addEventListener('click', function(e) {
         }).then(data => {
             console.log(data)
             result.classList.remove('close');
-            if(data === 'success') {
+            if(data !== 'success') {
+                console.log(data);
                 signaler.classList.remove('bad');
                 signaler.classList.add('ok');
                 resIcon.classList.remove('fa-times');
